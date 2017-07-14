dc9a9299459b7ed9d2b67bb4d428377dab530356
Hristo Tanev <hristotanev7@gmail.com>
2017-07-14 17:01:56 +0100

    Added utils.js file.

--- server/routes.js
+++ server/routes.js
@@ -1,42 +1,5 @@
-let romanNumerals = require('roman-numerals')
-
-let Numeral = require('../models/Numeral')
 let isValidArabic = require('../utils')
-
-const convertNumeral = (response, type, numeral) => {
-  let number = '', n = new Numeral()
-  try {
-    if (type == 'roman') {
-      number = romanNumerals.toArabic(numeral.toUpperCase())
-    } else {
-      number = romanNumerals.toRoman(numeral.toUpperCase())
-    }
-
-    if (type == 'roman' || isValidArabic(numeral)) {
-      n = new Numeral({ type, input_value: numeral, converted_value: number.toString() })
-      n.save()
-      response.json({ inputValue: numeral, convertedValue: number.toString() })
-    } else {
-      response.send(400)
-    }
-  } catch (error) {
-    response.send(400)
-  }
-}
-
-const checkAndConvert = (response, type, number) => {
-  Numeral.findOne({ type, input_value: number }, (error, numeral) => {
-    if (error) {
-      response.send(400)
-    }
-
-    if (numeral == null) {
-      convertNumeral(response, type, number)
-    } else {
-      response.json({ inputValue: numeral.input_value, convertedValue: numeral.converted_value })
-    }
-  })
-}
+let checkAndConvert = require('../utils')
 
 const handleRoutes = (server) => {
   server.get('/roman/:number', (request, response, next) => {
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
+let romanNumerals = require('roman-numerals')
+
+let Numeral = require('./models/Numeral')
+
 function isValidArabic(number) {
   for (i of number) {
     if (i < '0' || i > '9') {
@@ -8,4 +12,40 @@ function isValidArabic(number) {
   return true
 }
 
+const convertNumeral = (response, type, numeral) => {
+  let number = '', n = new Numeral()
+  try {
+    if (type == 'roman') {
+      number = romanNumerals.toArabic(numeral.toUpperCase())
+    } else {
+      number = romanNumerals.toRoman(numeral.toUpperCase())
+    }
+
+    if (type == 'roman' || isValidArabic(numeral)) {
+      n = new Numeral({ type, input_value: numeral, converted_value: number.toString() })
+      n.save()
+      response.json({ inputValue: numeral, convertedValue: number.toString() })
+    } else {
+      response.send(400)
+    }
+  } catch (error) {
+    response.send(400)
+  }
+}
+
+const checkAndConvert = (response, type, number) => {
+  Numeral.findOne({ type, input_value: number }, (error, numeral) => {
+    if (error) {
+      response.send(400)
+    }
+
+    if (numeral == null) {
+      convertNumeral(response, type, number)
+    } else {
+      response.json({ inputValue: numeral.input_value, convertedValue: numeral.converted_value })
+    }
+  })
+}
+
 module.exports = isValidArabic
+module.exports = checkAndConvert

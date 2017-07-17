d26cfc979666930bcf8ee3b2a84c64b3ae02dcaf
Hristo Tanev <hristotanev7@gmail.com>
2017-07-17 14:53:35 +0100

    Made further fixes.

--- test/test-d-removeAll.js
+++ test/test-d-removeAll.js
@@ -1,18 +1,19 @@
-let should = require('chai').should()
 let expect = require('chai').expect
 let supertest = require('supertest')
 let api = supertest('http://localhost:3000')
 
+const expectStatusToBe = (status, done) => {
+  api.get('/remove/all')
+     .set('Accept', 'application/json')
+     .expect(status, done)
+}
+
 describe('Serving DELETE /remove/all', () => {
   it('successful removal of all elements in the DB', (done) => {
-    api.get('/remove/all')
-       .set('Accept', 'application/json')
-       .expect(200, done)
+    expectStatusToBe(200, done)
   })
 
   it('unsuccessful removal of all elements in the DB', (done) => {
-    api.get('/remove/all')
-       .set('Accept', 'application/json')
-       .expect(500, done)
+    expectStatusToBe(500, done)
   })
 })
